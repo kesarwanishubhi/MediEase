0ca6cd570eee9732071d5c9a4ebb5e9b60b5f000
kesarwanishubhi <shubhikesarwani1234@gmail.com>
2025-02-16 19:23:10 +0530

    Initializing the project

---
+++ src/main/java/com/shubhi/mediease/MediEaseApplication.java
@@ -0,0 +1,13 @@
+package com.shubhi.mediease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediEaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediEaseApplication.class, args);
+    }
+
+}
